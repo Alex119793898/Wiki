2d6a3e4d1d263675625f6ad8bd1b6ff43a1221a1
csl119793898 <119793898@qq.com>
2022-04-07 15:08:12 +0800

    3-6 增加通用返回类CommonResp,以后所有后端接口全部都是CommonResp类型

--- src/main/java/com/caoliang/wiki/controller/EbookController.java
+++ src/main/java/com/caoliang/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.caoliang.wiki.controller;
 
 import com.caoliang.wiki.pojo.Ebook;
+import com.caoliang.wiki.resp.CommonResp;
 import com.caoliang.wiki.service.EbookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +18,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list(){
-        return ebookService.list();
+    public CommonResp list(){
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
