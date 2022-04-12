8c4b606f57ae5cf5c78549b06387037bea7351dd
csl119793898 <119793898@qq.com>
2022-04-12 14:56:39 +0800

    5-4 列表查询接口支持动态SQL

--- src/main/java/com/caoliang/wiki/service/EbookService.java
+++ src/main/java/com/caoliang/wiki/service/EbookService.java
@@ -8,6 +8,7 @@ import com.caoliang.wiki.req.EbookResp;
 import com.caoliang.wiki.util.CopyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import java.util.List;
 
@@ -20,7 +21,11 @@ public class EbookService {
     public List<EbookResp> list(EbookReq req){
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%" + req.getName() + "%");
+
+        if(!ObjectUtils.isEmpty(req.getName())){
+            criteria.andNameLike("%" + req.getName() + "%");
+        }
+
 
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
