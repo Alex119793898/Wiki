6e21c551a3e0b7c7b2f5bc73b15bbf3b3c1a7b89
csl119793898 <119793898@qq.com>
2022-04-12 18:35:30 +0800

    5-7 增加拦截器,打印接口耗时

--- src/main/java/com/caoliang/wiki/config/MvcConfig.java
+++ src/main/java/com/caoliang/wiki/config/MvcConfig.java
@@ -24,12 +24,6 @@ public class MvcConfig implements WebMvcConfigurer {
     * 重写该方法
     * 往拦截器链中添加拦截器
     * @params registry 拦截器链
-    *   Spring之Interceptor之path patterns路径匹配规则
-	*		一个*：只匹配字符，不匹配路径（/）
-	   *		两个**：匹配字符，和路径（/）
-	   *			- /**： 匹配所有路径
-     *			- /admin/**：匹配 /admin/ 下的所有路径
-  	   *			- /secure/*：只匹配 /secure/user，不匹配 /secure/user/info
     * */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
