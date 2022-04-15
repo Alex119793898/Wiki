d3575300358f15d0519c8200c273ada85133c5f6
csl119793898 <119793898@qq.com>
2022-04-15 20:33:08 +0800

    *Tips* 屏蔽console

--- web/src/main.ts
+++ web/src/main.ts
@@ -11,14 +11,14 @@ axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 
 /*axios拦截器*/
 axios.interceptors.request.use(config=>{
-    console.log('请求参数:',config);
+    //console.log('请求参数:',config);
     return config;
 },error => {
     return Promise.reject(error)
 })
 
 axios.interceptors.response.use(response=>{
-    console.log('响应数据:',response);
+    //console.log('响应数据:',response);
     return response;
 },error => {
     return Promise.reject(error)
