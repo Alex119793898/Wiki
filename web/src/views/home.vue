c5114ccefe50996786a54121741960ef5c9c6ec1
csl119793898 <119793898@qq.com>
2022-04-21 15:11:29 +0800

    7-7 首页显示分类菜单

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -2,43 +2,20 @@
   <a-layout>
     <a-layout-sider width="200" style="background: #fff">
       <a-menu
-          v-model:selectedKeys="selectedKeys2"
-          v-model:openKeys="openKeys"
           mode="inline"
           :style="{ height: '100%', borderRight: 0 }"
+          @click="handleMenuClick"
       >
-        <a-sub-menu key="sub1">
+        <a-menu-item key="welcome">
+          <router-link to="/">
+            <span>欢迎</span>
+          </router-link>
+        </a-menu-item>
+        <a-sub-menu v-for="item in levelTree" :key="item.id">
           <template #title>
-              <span>
-                subnav 1
-              </span>
+              <span>{{item.name}}</span>
           </template>
-          <a-menu-item key="1">option1</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-              <span>
-                subnav 2
-              </span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
-              <span>
-                subnav 3
-              </span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
+          <a-menu-item v-for="child in item.children" :key="child.id">{{child.name}}</a-menu-item>
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
@@ -68,9 +45,11 @@
   </a-layout>
 </template>
 
-<script lang="ts">
+<script>
 import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
-import axios from "axios";
+import axios from 'axios';
+import { message } from 'ant-design-vue';
+import { array2Tree} from "@/util/tool";
 
 export default defineComponent({
   name: 'HomeView',
@@ -80,24 +59,47 @@ export default defineComponent({
   setup() {
 
     const ebookList = ref();
-
-    onMounted(()=>{
+    const handleQuery = ()=>{
       axios.get("/ebook/list",{params:{page:1,size:1000}}).then(res=>{
         ebookList.value = res.data.content.list;
       })
+    }
+
+    const levelTree = ref([]);
+
+    /*侧边栏菜单分类数据*/
+    const handleQueryCategory = () =>{
+      axios.get("/category/all").then(res=>{
+        const data = res.data;
+        if(data.success){
+          levelTree.value = array2Tree(data.content,0);
+
+          console.log("树形结构:",levelTree.value)
+        }else{
+          message.warning(data.message)
+        }
+      })
+    }
+
+    const handleMenuClick = (event)=>{
+      console.log(event)
+    }
+
+    onMounted(()=>{
+      handleQuery();
+      handleQueryCategory();
     })
 
     return {
-      selectedKeys1: ref<string[]>(['2']),
-      selectedKeys2: ref<string[]>(['1']),
-      collapsed: ref<boolean>(false),
-      openKeys: ref<string[]>(['sub1']),
       ebookList,
+      levelTree,
+      handleMenuClick,
+
       actions: [
         { type: 'StarOutlined', text: '156' },
         { type: 'LikeOutlined', text: '156' },
         { type: 'MessageOutlined', text: '2' },
-      ]
+      ],
     };
 
 
